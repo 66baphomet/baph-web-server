d1831eb2769bad98c36c6aafe470fb233f9321a7
Sabbir Ahmed <sabbirahmedd.66@gmail.com>
2020-07-14 23:44:56 +0600

    testing git

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//git push
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
@@ -23,7 +24,9 @@ app.get('', (req, res)=>{
 })
 
 app.get('/about', (req, res)=>{
-    res.render('about')
+    res.render('about',{
+        name: 'Dipto'
+    })
 })
 
 app.get('/help', (req, res)=>{
