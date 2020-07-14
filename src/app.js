7b8b61139d66ad4c7f43c5658f328f43940fa298
Sabbir Ahmed <sabbirahmedd.66@gmail.com>
2020-07-14 23:02:57 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+//defining the public, views and partials path
+
+const publicDirectory = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setting the public, views and partials path
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setting up static directory to serve
+app.use(express.static(publicDirectory))
+
+app.get('', (req, res)=>{
+    res.render('index')
+})
+
+app.get('/about', (req, res)=>{
+    res.render('about')
+})
+
+app.get('/help', (req, res)=>{
+    res.render('help')
+})
+
+app.listen(3000, ()=>{
+    console.log('Server is upto 3000')
+})
